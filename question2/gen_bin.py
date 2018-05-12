fa36ee7d088cd41deeb1b29a0ede88699e1ba84c
Amy Xiao <amy.xiao@mail.utoronto.ca>
2018-05-12 16:48:42 -0400

    added alternate implementation of pascal triangle

--- question2/gen_bin.py
+++ question2/gen_bin.py
@@ -4,19 +4,31 @@ def main():
     n = 5
     k = 3
     #print gen_bin(5, 3)  
-    gen_pascal(5)
+    #gen_pascal(5)
+    gen_pascal2(5)
 
 def gen_bin(n, k):
-    if k < 1:
-        print "Invalid k."
     if k == 0:
-        return 1
+        return 1 
+    if k < 1:
+        #print "Invalid k."
+        return
+    #if k == 0:
+    #    return 1
     else:
         n_fac = factorial(n)
         k_fac = factorial(k)
         nk_fac = factorial(n-k)
         #print n_fac, k_fac, nk_fac
         return int(n_fac / (k_fac*nk_fac))
+
+def gen_pascal2(n_lines):
+    for line in xrange(n_lines):
+        print "line ", line
+        for e in xrange(line+1):
+            print(gen_bin(line, e))
+       
+    
 
 def gen_pascal(n_lines):
     triangle = [[] for line in xrange(n_lines)]
