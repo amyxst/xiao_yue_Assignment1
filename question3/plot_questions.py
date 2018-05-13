82548cbe98d6ab4c4b1053bc2b30f762e1c6a36e
Amy Xiao <amy.xiao@mail.utoronto.ca>
2018-05-13 14:24:13 -0400

    plotted point spread function

---
+++ question3/plot_questions.py
@@ -0,0 +1,61 @@
+from scipy import integrate
+from scipy.special import jv
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    # Part a)
+    print("a) Implementing Bessel function of the first kind of order, x=4")
+    print "Implemented routine result: ", bessel(1,4) 
+    print "Scipy result: ", jv(1,4)
+    
+    #plot_bessel()
+
+    # Part b)
+    #print point_spread_x(0.3)
+    plot_point_spread()
+
+def bessel(m, x):
+    f = lambda theta: math.cos(m*theta - x*math.sin(theta))
+    return (1 / math.pi) * integrate.quad(f, 0, math.pi)[0]
+
+def plot_bessel(m_range=[0,5], x_range=[-10,10]):
+    plt.figure()
+
+    x = np.linspace(x_range[0], x_range[1], 500)
+    y_f = np.vectorize(bessel)
+    
+    for m in xrange(m_range[0], m_range[1]+1):
+        plt.plot(x, y_f(m, x), label="m = {0}".format(m))
+
+    plt.title("a) Bessel function behaviour of the first kind of order m for selected x")
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("$J_m(x)$")
+    plt.show()
+
+def point_spread_x(q, a=0.1, lmda=0.1, R=0.7):
+    return (2*math.pi*a*q)/(lmda*R)
+
+def point_spread(q, Inaut=40):
+    x = point_spread_x(q)
+    return Inaut*((2*bessel(1,x))/x)**2
+
+def plot_point_spread():
+    q = np.linspace(-0.3, 0.3, 300)
+    ps_x = np.vectorize(point_spread_x)
+    ps_y = np.vectorize(point_spread)
+
+    x = ps_x(q)
+    y = ps_y(x)
+
+    plt.figure()
+    plt.title("b) Point Spread Function")
+    plt.xlabel("x")
+    plt.ylabel("I(x)")
+    plt.plot(x, y)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
